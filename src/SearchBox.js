9f6db6f92806799b6df634ca03d3a937ee9389f2
om shankar <om8003golu@gmail.com>
2022-12-15 18:59:59 +0530

    second commit

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,5 +1,4 @@
-import React from 'react'
-
+import React, { useState} from 'react'
 import OutlinedInput from '@mui/material/OutlinedInput';
 import Button from '@mui/material/Button';
 import List from '@mui/material/List';
@@ -10,14 +9,23 @@ import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
 
 const SearchBox = () => {
+    const [searchText, setSearchText] = useState("");
   return (
     <div style={{display:'flex', flexDirection:'column'}}>
         <div style={{display:'flex'}}>
             <div style={{flex:1}}>
-                <OutlinedInput style={{width:'100%'}} />
+                <OutlinedInput 
+                    style={{width:'100%'}}
+                    value={searchText}
+                    onChange={(event) => setSearchText(event.target.value)}
+                />
             </div>
-            <div >
-                <Button variant="contained" style={{ padding:'15px 12px'}}>Search</Button>
+            <div>
+                <Button 
+                    variant="contained" 
+                    style={{ padding:'15px 12px'}} 
+                    onClick={()=>{}}
+                >Search</Button>
             </div>
         </div>
 
